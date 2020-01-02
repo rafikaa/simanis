a4d550c184e7a2ff7b84c48c6ebe6de713baa2de
Dwi Aji Kurniawan <dwi.aji.kurniawan@gmail.com>
2020-01-02 16:08:46 +0700

    UI (#1)
    * initialize index and signin pages
    
    * delete yarn.lock
    
    * delete package-lock
    
    * activate data table at home page
    
    * data unit page
    
    * create page input data unit
    
    * create data nphr page
    
    * add nphr trend table and chart
    
    * create input data nphr page
    
    * remove placeholder in input data unit
    
    * create laporan page
    
    * deactivate searching datatable
    
    * create download page
    
    * deactivate searching at data unit page
    
    * create page analisa nphr
    
    * add datatable locally
    
    * create pemakaian sendiri page
    
    * create maturity level page
    
    * rearrange charts
    
    Co-authored-by: aminirafika@gmail.com <aminirafika@gmail.com>

---
+++ routes/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'SIMANIS' });
+});
+
+router.get('/signin', function(req, res, next) {
+  res.render('signin', { title: 'Sign In' });
+});
+
+router.get('/data-unit', function(req, res, next) {
+  res.render('data-unit', { title: 'Data Unit' });
+});
+
+router.get('/input-data-unit', function(req, res, next) {
+  res.render('input-data-unit', { title: 'Data Unit' });
+});
+
+router.get('/data-nphr', function(req, res, next) {
+  res.render('data-nphr', { title: 'Data NPHR' });
+});
+
+router.get('/input-data-nphr', function(req, res, next) {
+  res.render('input-data-nphr', { title: 'Data NPHR' });
+});
+
+router.get('/analisa-nphr', function(req, res, next) {
+  res.render('analisa-nphr', { title: 'Analisa NPHR' });
+});
+
+router.get('/input-data-analisa-nphr', function(req, res, next) {
+  res.render('input-data-analisa-nphr', { title: 'Data Analisa NPHR' });
+});
+
+router.get('/pemakaian-sendiri', function(req, res, next) {
+  res.render('pemakaian-sendiri', { title: 'Pemakaian Sendiri' });
+});
+
+router.get('/input-pemakaian-sendiri', function(req, res, next) {
+  res.render('input-pemakaian-sendiri', { title: 'Pemakaian Sendiri' });
+});
+
+router.get('/maturity-level', function(req, res, next) {
+  res.render('maturity-level', { title: 'Maturity Level' });
+});
+
+router.get('/input-maturity-level', function(req, res, next) {
+  res.render('input-maturity-level', { title: 'Maturity Level' });
+});
+
+router.get('/laporan', function(req, res, next) {
+  res.render('laporan', { title: 'Laporan' });
+});
+
+router.get('/download', function(req, res, next) {
+  res.render('download', { title: 'Download' });
+});
+
+module.exports = router;
