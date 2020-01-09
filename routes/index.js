b880b7421182c74e36adfd495981fb6aec9a7d63
Rafika Amini <rafika@wonderlabs.io>
2020-01-10 06:22:10 +0700

    change sidebar icons to themify

--- routes/index.js
+++ routes/index.js
@@ -61,4 +61,12 @@ router.get('/download', function(req, res, next) {
   res.render('download', { title: 'Download' });
 });
 
+router.get('/404', function(req, res, next) {
+  res.render('404', { title: 'Error' });
+});
+
+router.get('/500', function(req, res, next) {
+  res.render('500', { title: 'Error' });
+});
+
 module.exports = router;
