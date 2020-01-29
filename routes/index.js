d790bfe94aa07f8d408560212c9eab31014f03d8
Dwi Aji Kurniawan <dwi.aji@traveloka.com>
2020-01-30 06:04:44 +0700

    Standardize the layout template

--- routes/index.js
+++ routes/index.js
@@ -16,47 +16,47 @@ router.get('/', isAuthenticated, (req, res, next) => {
 });
 
 router.get('/analisa-nphr', function(req, res, next) {
-  res.render('analisa-nphr', { title: 'Analisis NPHR' });
+  res.render('analisa-nphr', { layout: 'dashboard', title: 'Analisis NPHR' });
 });
 
 router.get('/input-data-analisa-nphr', function(req, res, next) {
-  res.render('input-data-analisa-nphr', { title: 'Data Analisis NPHR' });
+  res.render('input-data-analisa-nphr', { layout: 'dashboard', title: 'Data Analisis NPHR' });
 });
 
 router.get('/pemakaian-sendiri', function(req, res, next) {
-  res.render('pemakaian-sendiri', { title: 'Pemakaian Sendiri' });
+  res.render('pemakaian-sendiri', { layout: 'dashboard', title: 'Pemakaian Sendiri' });
 });
 
 router.get('/input-pemakaian-sendiri', function(req, res, next) {
-  res.render('input-pemakaian-sendiri', { title: 'Pemakaian Sendiri' });
+  res.render('input-pemakaian-sendiri', { layout: 'dashboard', title: 'Pemakaian Sendiri' });
 });
 
 router.get('/maturity-level', function(req, res, next) {
-  res.render('maturity-level', { title: 'Maturity Level' });
+  res.render('maturity-level', { layout: 'dashboard', title: 'Maturity Level' });
 });
 
 router.get('/maturity-level-x', function(req, res, next) {
-  res.render('maturity-level-x', { title: 'Maturity Level' });
+  res.render('maturity-level-x', { layout: 'dashboard', title: 'Maturity Level' });
 });
 
 router.get('/input-maturity-level', function(req, res, next) {
-  res.render('input-maturity-level', { title: 'Maturity Level' });
+  res.render('input-maturity-level', { layout: 'dashboard', title: 'Maturity Level' });
 });
 
 router.get('/input-maturity-level-target', function(req, res, next) {
-  res.render('input-maturity-level-target', { title: 'Maturity Level' });
+  res.render('input-maturity-level-target', { layout: 'dashboard', title: 'Maturity Level' });
 });
 
 router.get('/laporan', function(req, res, next) {
-  res.render('laporan', { title: 'Laporan' });
+  res.render('laporan', { layout: 'dashboard', title: 'Laporan' });
 });
 
 router.get('/download', function(req, res, next) {
-  res.render('download', { title: 'Download' });
+  res.render('download', { layout: 'dashboard', title: 'Download' });
 });
 
 router.get('/input-download', function(req, res, next) {
-  res.render('input-download', { title: 'Download' });
+  res.render('input-download', { layout: 'dashboard', title: 'Download' });
 });
 
 router.get('/404', function(req, res, next) {
