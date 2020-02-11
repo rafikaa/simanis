a3a20795c2af6b096dbe2f7f25eaa716896250bb
Dwi Aji Kurniawan <dwi.aji@traveloka.com>
2020-02-11 21:53:44 +0700

    Form working, minus bobot

--- routes/maturity-level.js
+++ routes/maturity-level.js
@@ -87,18 +87,17 @@ router.post('/target', isAuthenticated, async (req, res, next) => {
         semester,
         tahun,
         upk,
-        averageTarget: avgTarget,
       });
     }
 
     ml.pengumpulanDataEfisiensi.target = pengumpulanDataEfisiensi;
-    ml.perhitunganPerformanceTest.target = pengumpulanDataEfisiensi;
-    ml.pemodelan.target = pengumpulanDataEfisiensi;
-    ml.heatRateAnalysis.target = pengumpulanDataEfisiensi;
-    ml.auxiliaryPowerAnalysis.target = pengumpulanDataEfisiensi;
-    ml.rekomendasi.target = pengumpulanDataEfisiensi;
-    ml.pelaporanEfisiensi.target = pengumpulanDataEfisiensi;
-    ml.monitoringPostProgram.target = pengumpulanDataEfisiensi;
+    ml.perhitunganPerformanceTest.target = perhitunganPerformanceTest;
+    ml.pemodelan.target = pemodelan;
+    ml.heatRateAnalysis.target = heatRateAnalysis;
+    ml.auxiliaryPowerAnalysis.target = auxiliaryPowerAnalysis;
+    ml.rekomendasi.target = rekomendasi;
+    ml.pelaporanEfisiensi.target = pelaporanEfisiensi;
+    ml.monitoringPostProgram.target = monitoringPostProgram;
     ml.averageTarget = avgTarget;
 
     await ml.save();
@@ -127,9 +126,79 @@ router.get('/realisasi', isAuthenticated, async (req, res, next) => {
   });
 });
 
+const parseFormData = (body) => {
+  const formData = {};
+  for (let formKey of Object.keys(body)) {
+    const keys = formKey.replace(/]/g, '');
+    const [key, idx1, idx2] = keys.split('[');
+    if (!formData[key]) formData[key] = [];
+    if (!formData[key][Number(idx1)]) formData[key][Number(idx1)] = [];
+    formData[key][Number(idx1)][Number(idx2)] = body[formKey] === 'y';
+  }
+  return formData;
+};
+
+const weight = {
+  pengumpulanDataEfisiensi: [
+    [0.2, 0.8],
+    [0.3, 0.3, 0.4],
+    [0.2, 0.3, 0.3, 0.2],
+    [0.5, 0.5],
+    [0.4, 0.6],
+  ],
+};
+
+const calculateScores = (formData) => {
+  const scores = {};
+  for (let mlCat of Object.keys(formData)) {
+    for (let [i, ival] of formData[mlCat].entries()) {
+      for (let [j, jval] of ival.entries()) {
+        if (weight[mlCat] && weight[mlCat][i] && weight[mlCat][i][j]) {
+          if (!scores[mlCat]) {
+            scores[mlCat] = jval * weight[mlCat][i][j];
+          } else {
+            scores[mlCat] += jval * weight[mlCat][i][j];
+          }
+        }
+      }
+    }
+  }
+};
+
 router.post('/realisasi', isAuthenticated, async (req, res, next) => {
   const isAdmin = req.user.accountType === 'ADMIN';
   const units = await getUnitList(req.user);
+
+  const formData = parseFormData(req.body);
+  const scores = calculateScores(formData);
+  console.log(formData);
+  console.log(scores);
+  const sumRealisasi = Object.values(scores).reduce((a, b) => a + b, 0);
+  const averageRealisasi = (sumRealisasi / Object.values(scores).length) || 0;
+
+  let ml = await MaturityLevel.findOne({ semester, tahun, upk });
+
+  if (!ml) {
+    ml = new MaturityLevel({
+      semester,
+      tahun,
+      upk,
+    });
+  }
+
+  ml.pengumpulanDataEfisiensi.realisasi = scores.pengumpulanDataEfisiensi;
+  ml.perhitunganPerformanceTest.realisasi = scores.perhitunganPerformanceTest;
+  ml.pemodelan.realisasi = scores.pemodelan;
+  ml.heatRateAnalysis.realisasi = scores.heatRateAnalysis;
+  ml.auxiliaryPowerAnalysis.realisasi = scores.auxiliaryPowerAnalysis;
+  ml.rekomendasi.realisasi = scores.rekomendasi;
+  ml.pelaporanEfisiensi.realisasi = scores.pelaporanEfisiensi;
+  ml.monitoringPostProgram.realisasi = scores.monitoringPostProgram;
+  ml.averageRealisasi = averageRealisasi;
+
+  await ml.save();
+
+  // TODO upload file
 
   return res.render('maturity-level/realisasi', {
     layout: 'dashboard',
